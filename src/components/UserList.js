bdf7f9c32dcce6af026520f0b5d38dc5e65da60a
Aaron Stockdale <shadowstock76@gmail.com>
2022-12-30 11:41:08 -0500

    Got usernames to show up in UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { requestAllUsers, requestAddFriend } from "./Requests";
+import { requestUserInfo, requestAllUsers, requestAddFriend } from "./Requests";
 
-export const Users = ({ token }) => {
+export const Users = ({ token, username }) => {
   const [UserList, setUserList] = useState([]);
   const [buttonText, setButtonText] = useState("Befriend");
 
@@ -9,11 +9,10 @@ export const Users = ({ token }) => {
     requestAllUsers(token).then((res) => setUserList(res.data));
   }, [token]);
 
-  const handleBefriend = (friendId) => {
-    requestAddFriend(token, friendId).then(() => {
+  const handleBefriend = (userId) => {
+    requestAddFriend(token, userId).then(() => {
       setUserList((prevFriendList) =>
-        prevFriendList.filter((friend) => friend.id !== friendId)
-      );
+        prevFriendList.concat([{ id: userId }]));
     });
   };
 
@@ -34,7 +33,7 @@ export const Users = ({ token }) => {
           {UserList.map((user, idx) => (
             <div key={idx}>
               <div className="card">
-                <p>{user.user}</p>
+                <p>{user.username}</p>
                 {/* Add the befriend button to the card */}
                 <button
                   onClick={() => handleBefriend(user.id)}
@@ -51,6 +50,7 @@ export const Users = ({ token }) => {
                     >
                       <path fill="none" d="M0 0h24v24H0z" />
                       <path d="M17.841 15.659l.176.177.178-.177a2.25 2.25 0 0 1 3.182 3.182l-3.36 3.359-3.358-3.359a2.25 2.25 0 0 1 3.182-3.182zM12 14v2a6 6 0 0 0-6 6H4a8 8 0 0 1 7.75-7.996L12 14zm0-13c3.315 0 6 2.685 6 6a5.998 5.998 0 0 1-5.775 5.996L12 13c-3.315 0-6-2.685-6-6a5.998 5.998 0 0 1 5.775-5.996L12 1zm0 2C9.79 3 8 4.79 8 7s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z" />
+
                     </svg>
                     <span className="button-text">{buttonText}</span>
                   </div>
