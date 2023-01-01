03ee6ff6a0b7eb7b89e7d34f990c6482ade527f1
Your Name <alobryan2@gmail.com>
2023-01-01 13:06:05 -0500

    added some request for weeklogs as well as make basic component working

--- src/components/OtherUserRecords.js
+++ src/components/OtherUserRecords.js
@@ -19,7 +19,7 @@ export const OtherUserRecords = ({ token }) => {
                 <div className='UserRecords'>{recordList.map((record, idx) => (
                     <div key={idx}>
                         {/* would like to hav name of habit serialized */}
-                        <p>Record for {record.week_reflection } on {record.date}</p>
+                        <p>Record for {record.habit_name } on {record.date}</p>
                         {/* This is where link to record detail would go */}
                         <Link  to='/records/:recordId' state={{ id: record.id }}> See record detail</Link>
                         {/* maybe a link to the users profile */}
