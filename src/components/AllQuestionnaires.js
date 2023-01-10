d1da7d5b38e1bdabf98ce743dcc5b55b05bf085b
Your Name <alobryan2@gmail.com>
2023-01-10 10:58:44 -0500

    basic habit page

--- src/components/AllQuestionnaires.js
+++ src/components/AllQuestionnaires.js
@@ -1,7 +1,8 @@
 import { requestAllQuestionnaires } from './Requests'
 import { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
-
+import { DoorOpen } from "@styled-icons/bootstrap/DoorOpen"
+ 
 export const AllQuestionnaires = ({ token }) => {
     const [questionnaireList, setQuestionnaire] = useState([])
 
@@ -15,8 +16,12 @@ export const AllQuestionnaires = ({ token }) => {
             <h1>Your habits</h1>
             <div className='list-of-habits'>{questionnaireList.map((questionnaire, idx) => (
                 <div className='each-habit' key={idx}>
-                    <p>{questionnaire.habit_name} started on {questionnaire.date}</p>
-                    <Link  to='/habit-page/:id' state={{ id: questionnaire.id }}> Habit Info</Link>
+                    <div className='status-habit-y'>
+                        <h3>{questionnaire.habit_name}</h3>
+                        {questionnaire.status ? <p>active</p>: <p>unactive</p> }
+                    </div>
+                    
+                    <Link  to='/habit-page/:id' state={{ id: questionnaire.id }}> <DoorOpen className='icon-temp'/></Link>
                 </div>
             ))}
             </div>    
